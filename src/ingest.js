f047d8dfa33053170a3670fb127a1fa45afbfaa7
aaron.zhang <aaron.zhang@wanews.com.au>
2023-06-17 03:53:16 +0800

    spit doc file

--- src/ingest.js
+++ src/ingest.js
@@ -1,15 +1,22 @@
 import { UnstructuredLoader } from 'langchain/document_loaders/fs/unstructured'
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+// import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+
 import { getAbsolutePath } from './util.js'
 
 // Feed MD file
 const mdFile = getAbsolutePath('./data/doc.md')
 const unstructuredLoader = new UnstructuredLoader(mdFile)
-const mdDoc = await unstructuredLoader.load()
+const rawDoc = await unstructuredLoader.load()
 
 // Feed pdf file - will need `pdf-parse` pkg
 // const pdfFile = getAbsolutePath('./data/oauth2.pdf')
 // const pdfLoader = new PDFLoader(pdfFile)
 // const pdfDoc = await pdfLoader.load()
 
-console.log(mdDoc)
+// console.log(mdDoc)
+
+const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
+const docs = await splitter.splitDocuments(rawDoc)
+
+console.log(docs)
